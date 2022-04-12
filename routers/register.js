4955cba854ead0c117d232e7f462b851a533c45e
jlmoer <jlmoerdijk@gmail.com>
2022-04-12 15:57:58 +0200

    register route

--- routers/register.js
+++ routers/register.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+
+// Create user (signup)
+router.post("/", async (req, res) => {
+    // Password hashing
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    try {
+        await Postgres.query(
+            "INSERT INTO users(last_name, first_name, email, city, is_admin, password) VALUES ($1, $2, $3, $4, $5)",
+            [req.body.firstName, req.body.lastName, req.body.email, req.body.city, req.body.isAdmin, hashedPassword]
+        );
+    } catch (err) {
+        return res.status(400).json({ message: err });
+    }
+    res.status(201).json({ message: "User created" });
+});
 
 // Export route
 module.exports = router;
