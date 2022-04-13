a4188fad83fa300dfcb3b9a1fadda3689142d570
jlmoer <jlmoerdijk@gmail.com>
2022-04-13 16:11:41 +0200

    query params added to the actions GET route

--- routers/actions.js
+++ routers/actions.js
@@ -12,7 +12,7 @@ router.post("/", isLoggedIn, async (req, res) => {
     const { title, type, description, address, date, time, city } = req.body;
     try {
         await Postgres.query("INSERT INTO actions(title, type, description, address, date, time, organiser_id, city) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-            [title, type, description, address, date, time, req.data.id, city]);
+            [title, type, description, address, date, time, req.data.id, city.toLowerCase()]);
     } catch (err) {
         res.status(400).json({ message: err });
     }
@@ -21,19 +21,35 @@ router.post("/", isLoggedIn, async (req, res) => {
     });
 });
 
-// Get all actions
-router.get("/", async (_req, res) => {
+// Get all actions or filter actions by request query params
+router.get("/", async (req, res) => {
+    const queryKeys = Object.keys(req.query);
+    if (queryKeys.length === 0) {
+        try {
+            const actions = await Postgres.query("SELECT * FROM actions");
+            return res.status(200).json({
+                data: actions.rows,
+            })
+        } catch (err) {
+            return res.status(400).json({
+                message: err,
+            });
+        }
+    }
+    let queryString = `SELECT * FROM actions WHERE ${queryKeys[0]}='${req.query[queryKeys[0]].toString().toLowerCase()}'`;
+    for (i = 1; i < queryKeys.length; i++) {
+        queryString += ` AND 
+        ${queryKeys[i]} = '${req.query[queryKeys[i]].toString().toLowerCase()}'`
+    };
+    console.log(queryString);
     try {
-        const actions = await Postgres.query("SELECT * FROM actions");
-        return res.status(200).json({
-            data: actions.rows,
-        })
+        const actions = await Postgres.query(queryString);
+        return res.status(200).json({ data: actions.rows });
     } catch (err) {
-        return res.status(400).json({
-            message: err,
-        });
+        return res.status(400).json({ message: err });
     }
 });
+
 // Get an action by id
 router.get("/:action_id", async (req, res) => {
     try {
@@ -54,7 +70,7 @@ router.patch("/:action_id", isLoggedIn, isOrganiser, async (req, res) => {
     let params = Object.keys(req.body);
     let sql = params.reduce((prev, curr, index) => {
         return index === 0 ? `${prev} ${curr} = '${req.body[curr]}'` :
-            `${prev}, ${curr} = '${req.body[curr]}'`
+            `${prev}, ${curr} = '${req.body[curr]}'`;
     }, queryStart) + queryEnd;
     try {
         await Postgres.query(sql, [req.params.action_id]);
