1768da155097feb98b415d39dabee0b0f10f33e6
jlmoer <jlmoerdijk@gmail.com>
2022-04-13 15:13:06 +0200

    Update action with router.patch

--- routers/actions.js
+++ routers/actions.js
@@ -9,10 +9,10 @@ const isOrganiser = require("../middlewares/isOrganiser");
 
 // Action creation (POST)
 router.post("/", isLoggedIn, async (req, res) => {
-    const { title, type, description, address, date, time } = req.body;
+    const { title, type, description, address, date, time, city } = req.body;
     try {
-        await Postgres.query("INSERT INTO actions(title, type, description, address, date, time, organiser_id) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-            [title, type, description, address, date, time, req.data.id]);
+        await Postgres.query("INSERT INTO actions(title, type, description, address, date, time, organiser_id, city) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+            [title, type, description, address, date, time, req.data.id, city]);
     } catch (err) {
         res.status(400).json({ message: err });
     }
@@ -45,7 +45,24 @@ router.get("/:action_id", async (req, res) => {
     } catch (err) {
         return res.status(400).json({ message: err });
     }
-})
+});
+
+// PATCH an action
+router.patch("/:action_id", isLoggedIn, isOrganiser, async (req, res) => {
+    let queryStart = "UPDATE actions SET ";
+    let queryEnd = " WHERE action_id=$1";
+    let params = Object.keys(req.body);
+    let sql = params.reduce((prev, curr, index) => {
+        return index === 0 ? `${prev} ${curr} = '${req.body[curr]}'` :
+            `${prev}, ${curr} = '${req.body[curr]}'`
+    }, queryStart) + queryEnd;
+    try {
+        await Postgres.query(sql, [req.params.action_id]);
+    } catch (err) {
+        return res.status(400).json({ message: err });
+    }
+    res.status(200).json({ message: "Action updated" });
+});
 
 // Delete action 
 router.delete("/:action_id", isLoggedIn, isOrganiser, async (req, res) => {
