7bc71b40d27aec2bbc36c57eae8ec693f01116bc
jlmoer <jlmoerdijk@gmail.com>
2022-04-13 17:00:49 +0200

    POST participant in router.post in action.js

--- routers/actions.js
+++ routers/actions.js
@@ -89,6 +89,37 @@ router.delete("/:action_id", isLoggedIn, isOrganiser, async (req, res) => {
     }
     res.status(200).json({ message: "Action deleted" });
 });
+// Join action (insert row into participants)
+router.post("/:action_id/join", isLoggedIn, async (req, res) => {
+    let action;
+    try {
+        action = await Postgres.query("SELECT status FROM actions WHERE action_id = $1", [req.params.action_id]);
+        if (action.rows.length === 0) {
+            return res.status(400).json({
+                message: "Action not found",
+            });
+        }
+    } catch (err) {
+        return res.status(400).json({
+            message: err,
+        });
+    }
+    // Check if an action is not terminated or cancelled (status is not 1 or 2)
+    if (action.rows[0].status !== 0) {
+        return res.status(400).json({
+            message: "Action is either cancelled or completed",
+        });
+    }
+    try {
+        await Postgres.query("INSERT INTO participants (user_id, action_id) VALUES ($1, $2)", [
+            req.data.id,
+            req.params.action_id
+        ]);
+        return res.status(200).json({ message: "You joined the action" });
+    } catch (err) {
+        return res.status(400).json({ message: err });
+    }
+});
 
 // Export route
 module.exports = router;
