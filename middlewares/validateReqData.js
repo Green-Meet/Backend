3013b1831e5f9ab8d28b4f64aff0d3edecbedf12
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-19 14:06:43 +0200

    Remove address required in register schema

--- middlewares/validateReqData.js
+++ middlewares/validateReqData.js
@@ -1,95 +1,101 @@
 const express = require("express");
 const app = express();
-const Joi = require("Joi");
+const Joi = require("joi");
 
 const registerSchema = Joi.object({
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().min(6).required(),
-    city: Joi.string().required(),
-    address: Joi.string().required(),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().min(6).required(),
+  city: Joi.string().required(),
 });
 const loginSchema = Joi.object({
-    email: Joi.string().required().email(),
-    password: Joi.string().min(6).required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().min(6).required(),
 });
 const actionSchema = Joi.object({
-    title: Joi.string().required(),
-    type: Joi.string().required(),
-    description: Joi.string().required(),
-    address: Joi.string().required(),
-    date: Joi.date().required(),
-    time: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})-([0-9]{2})\:([0-9]{2})$/).required(),
-    city: Joi.string().required(),
+  title: Joi.string().required(),
+  type: Joi.string().required(),
+  description: Joi.string().required(),
+  address: Joi.string().required(),
+  date: Joi.date().required(),
+  time: Joi.string()
+    .regex(/^([0-9]{2})\:([0-9]{2})-([0-9]{2})\:([0-9]{2})$/)
+    .required(),
+  city: Joi.string().required(),
 });
 const actionPatchSchema = Joi.object({
-    title: Joi.string(),
-    type: Joi.string(),
-    description: Joi.string(),
-    address: Joi.string(),
-    date: Joi.date(),
-    time: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})-([0-9]{2})\:([0-9]{2})$/).required(),
-    city: Joi.string(),
+  title: Joi.string(),
+  type: Joi.string(),
+  description: Joi.string(),
+  address: Joi.string(),
+  date: Joi.date(),
+  time: Joi.string()
+    .regex(/^([0-9]{2})\:([0-9]{2})-([0-9]{2})\:([0-9]{2})$/)
+    .required(),
+  city: Joi.string(),
 });
 
 const userPatchSchema = Joi.object({
-    firstName: Joi.string(),
-    lastName: Joi.string(),
-    email: Joi.string().email(),
-    city: Joi.string(),
-    address: Joi.string(),
-})
+  firstName: Joi.string(),
+  lastName: Joi.string(),
+  email: Joi.string().email(),
+  city: Joi.string(),
+  address: Joi.string(),
+});
 
 const regValidation = (req, res, next) => {
-    const validationResult = registerSchema.validate(req.body);
-    if (validationResult.error) {
-        return res.status(400).json({
-            message: validationResult.error,
-        });
-    }
-    next();
+  const validationResult = registerSchema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error,
+    });
+  }
+  next();
 };
 const loginValidation = (req, res, next) => {
-    const validationResult = loginSchema.validate(req.body);
-    if (validationResult.error) {
-        return res.status(400).json({
-            message: validationResult.error,
-        });
-    }
-    next();
+  const validationResult = loginSchema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error,
+    });
+  }
+  next();
 };
 const actionValidation = (req, res, next) => {
-    const validationResult = actionSchema.validate(req.body);
-    if (validationResult.error) {
-        return res.status(400).json({
-            message: validationResult.error,
-        });
-    }
-    next();
+  const validationResult = actionSchema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error,
+    });
+  }
+  next();
 };
 const actionPatchValidation = (req, res, next) => {
-    const validationResult = actionPatchSchema.validate(req.body);
-    if (validationResult.error) {
-        return res.status(400).json({
-            message: validationResult.error,
-        });
-    }
-    next();
+  const validationResult = actionPatchSchema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error,
+    });
+  }
+  next();
 };
 
 const userPatchValidation = (req, res, next) => {
-    const validationResult = userPatchSchema.validate(req.body);
-    if (validationResult.error) {
-        return res.status(400).json({
-            message: validationResult.error,
-        });
-    }
-    next();
+  const validationResult = userPatchSchema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error,
+    });
+  }
+  next();
 };
-
 
 //Export
 module.exports = {
-    regValidation, loginValidation, actionValidation, actionPatchValidation, userPatchValidation,
+  regValidation,
+  loginValidation,
+  actionValidation,
+  actionPatchValidation,
+  userPatchValidation,
 };
