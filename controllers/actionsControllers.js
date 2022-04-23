5a3a78ce12dd2ca4a8fb593b0e6ea06807e97831
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-23 18:36:23 +0200

    Simplify patch/modify action controller

--- controllers/actionsControllers.js
+++ controllers/actionsControllers.js
@@ -112,17 +112,54 @@ const getActionByOrganiserId = async (req, res) => {
 
 // PATCH an action
 const patchAction = async (req, res) => {
-  let queryStart = "UPDATE actions SET ";
-  let queryEnd = " WHERE action_id=$1";
-  let params = Object.keys(req.body);
-  let sql =
-    params.reduce((prev, curr, index) => {
-      return index === 0
-        ? `${prev} ${curr} = '${req.body[curr]}'`
-        : `${prev}, ${curr} = '${req.body[curr]}'`;
-    }, queryStart) + queryEnd;
+  // let queryStart = "UPDATE actions SET ";
+  // let queryEnd = " WHERE action_id=$1";
+  // let params = Object.keys(req.body);
+  // let sql =
+  //   params.reduce((prev, curr, index) => {
+  //     return index === 0
+  //       ? `${prev} ${curr} = '${req.body[curr]}'`
+  //       : `${prev}, ${curr} = '${req.body[curr]}'`;
+  //   }, queryStart) + queryEnd;
+  // try {
+  //   await Postgres.query(sql, [req.params.action_id]);
+  const {
+    title,
+    type,
+    description,
+    address,
+    beginDate,
+    endDate,
+    beginTime,
+    endTime,
+    city,
+  } = req.body;
+  console.log("BODY", req.body.beginDate);
+  console.log("BODY", req.body.endDate);
+  console.log("BODY", req.body.beginTime);
+  console.log("BODY", req.body.endTime);
+  console.log("CITY", req.body.city);
+
+  console.log("params", req.params.action_id);
+  console.log("data", req.data.id);
+
   try {
-    await Postgres.query(sql, [req.params.action_id]);
+    await Postgres.query(
+      "UPDATE actions SET title=$1, type=$2, description=$3, address=$4, begin_date=$5, end_date=$6, begin_time=$7, end_time=$8, city=$9 WHERE action_id=$10",
+      [
+        title,
+        type,
+        description,
+        address,
+        beginDate,
+        endDate,
+        beginTime,
+        endTime,
+        city.toLowerCase(),
+        req.params.action_id,
+      ]
+    );
+    console.log("OK");
   } catch (err) {
     return res.status(400).json({ message: err });
   }
