5d236abfdaf2a3bcd30d92e2e66a80f689d81d8d
fusbudget <andrewsanser@gmail.com>
2022-04-27 13:07:21 +0200

    improve registerController, add jwt, return first_name, last_name

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -1,21 +1,35 @@
 const bcrypt = require("bcrypt");
 const { Pool } = require("pg");
 const Postgres = new Pool({ ssl: { rejectUnauthorized: false } });
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET;
 
 // Register route
 const registerRoute = async (req, res) => {
-    // Password hashing
-    const hashedPassword = await bcrypt.hash(req.body.password, 12);
-    const { firstName, lastName, email, city } = req.body;
-    try {
-        await Postgres.query(
-            "INSERT INTO users(last_name, first_name, email, city, password) VALUES ($1, $2, $3, $4, $5)",
-            [firstName, lastName, email, city, hashedPassword]
-        );
-    } catch (err) {
-        return res.status(400).json({ message: err });
-    }
-    res.status(201).json({ message: "User created" });
+  // Password hashing
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+  const { firstName, lastName, email, city } = req.body;
+  try {
+    const result = await Postgres.query(
+      "INSERT INTO users(last_name, first_name, email, city, password) VALUES ($1, $2, $3, $4, $5)  RETURNING user_id, first_name, last_name",
+      [firstName, lastName, email, city, hashedPassword]
+    );
+    const {user_id, first_name, last_name} = result.rows[0];
+    const token = jwt.sign({ id: user_id }, secret);
+    return res
+      .cookie("jwt", token, {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 48),
+        httpOnly: true,
+        secure: false,
+      })
+      .status(201)
+      .json({
+        last_name: last_name,
+        first_name: first_name,
+      });
+  } catch (err) {
+    return res.status(400).json({ message: err });
+  }
 };
 
 // Export
